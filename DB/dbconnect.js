f32e15849cd365394d04913abf135ffb3e553ac8
Iam Arjun <iamarjun537@gmail.com>
2024-06-27 18:01:39 +0545

    update again

--- DB/dbconnect.js
+++ DB/dbconnect.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		const atlasURL = "mongodb+srv://iamarjun537:badarni143@expresschatdb.mgcnrue.mongodb.net/?retryWrites=true&w=majority&appName=expresschatdb"
-		const localUrl = 'mongodb://localhost:27017/jschatdb'
-		await mongoose.connect(localUrl);
+		const dbUrl = "mongodb+srv://iamarjun537:badarni143@expresschatdb.mgcnrue.mongodb.net/?retryWrites=true&w=majority&appName=expresschatdb"
+		// const dbUrl = 'mongodb://localhost:27017/jschatdb'
+		await mongoose.connect(dbUrl);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
